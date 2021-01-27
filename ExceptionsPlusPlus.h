5374f050aeab50510069aab8b92c61ab56116f65
Ludwig Füchsl <ludwig@fuechsl.org>
2021-01-27 13:51:56 +0100

    Removed non ASCII chars from my name

--- ExceptionsPlusPlus.h
+++ ExceptionsPlusPlus.h
@@ -13,7 +13,7 @@
 *	===============================================================================
 *	MIT License
 *
-*	Copyright (c) 2021 Ludwig F�chsl
+*	Copyright (c) 2021 Ludwig Fuechsl
 *
 *	Permission is hereby granted, free of charge, to any person obtaining a copy
 *	of this software and associated documentation files (the "Software"), to deal
